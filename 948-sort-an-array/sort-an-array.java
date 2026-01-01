515b05a50f4a4ec2dec6c7f2a58daadcbf566633
miss-ankits <81066165+miss-ankits@users.noreply.github.com>
2026-01-01 19:13:26 +0530

    Time: 64 ms (22.86%) | Memory: 74.8 MB (96.00%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+         int n = nums.length;
+         mergeSort(nums,0,n-1);
+         return nums;
+    }
+    public static void mergeSort(int [] nums,int low,int high){
+        if(low>=high) return;
+         int mid=(high+low)/2;
+         mergeSort(nums,low,mid);
+         mergeSort(nums,mid+1,high); 
+         merge(nums,low,mid,high);    
+    }
+    public static void merge(int[] nums,int low,int mid,int high){
+        List<Integer> temp = new ArrayList<>();
+        int left=low; int right=mid+1;
+        while(left<=mid && right<=high){
+            if(nums[left]<=nums[right]){
+            temp.add(nums[left]);
+            left++;
+            }
+            else{
+                temp.add(nums[right]);
+                right++;
+            }
+        }
+        while(left<=mid){
+            temp.add(nums[left]);
+            left++;
+        }
+        while(right<=high){
+            temp.add(nums[right]);
+            right++;
+        }
+
+    for(int i=low;i<=high;i++){
+        nums[i]=temp.get(i-low);
+    }
+    }
+}
