e789eb39cb827b00f7f125ca12c33ea102b89912
miss-ankits <81066165+miss-ankits@users.noreply.github.com>
2026-01-06 19:06:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (30.81%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> arr=new ArrayList();
+        preorderTraverse(root,arr);
+        return arr;
+    }
+    public void preorderTraverse(TreeNode root,List<Integer> arr) {
+        if(root==null)
+        return;
+        arr.add(root.val);
+        preorderTraverse(root.left,arr);
+        preorderTraverse(root.right,arr);
+    }
+}
