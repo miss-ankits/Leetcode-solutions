ad612268809506fc81db0394365534af08a33d8d
miss-ankits <81066165+miss-ankits@users.noreply.github.com>
2026-02-09 22:35:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (84.86%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -5,18 +5,18 @@ class Solution {
             for(int j=i+1;j<n;j++){
                 int temp=matrix[i][j];
                 matrix[i][j]=matrix[j][i];
-                matrix[j][i]=temp;            
-            }
+                matrix[j][i]=temp;
+            } 
         }
         for(int i=0;i<n;i++){
             int left=0;int right=n-1;
             while(left<right){
-                int temp=matrix[i][left];
-                matrix[i][left]=matrix[i][right];
-                matrix[i][right]=temp;
-                left++;
-                right--;
-            }
+            int temp=matrix[i][left];
+            matrix[i][left]=matrix[i][right];
+            matrix[i][right]=temp;
+            left++;right--;
         }
+        }
+        
     }
     }
