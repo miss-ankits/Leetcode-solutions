cc77bebc95fed53c3df37fd9b116844dea838e82
miss-ankits <81066165+miss-ankits@users.noreply.github.com>
2026-02-15 18:38:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (59.71%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,22 +1,26 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         int left=m-1;
-        int right=0;
-        //int i=m+n-1;
-        // while(left<=0 && right<n){
-        //     if(nums1[left]>nums2[right]){
-        //         int temp=nums1[left];
-        //         nums1[left]=nums2[right];
-        //         nums2[right]=temp;
-        //         left--;
-        //         right++;
-        //     }else{
-        //         break;
-        //     }
-        // }
-        for(int i=0;i<n;i++){
-            nums1[m+i]=nums2[i];
+        int right=n-1;
+        int k=m+n-1; //start of 0
+        while(left>=0 && right>=0){
+            if(nums1[left]>nums2[right]){
+                nums1[k]=nums1[left];
+                left--;
+            }else{
+                nums1[k]=nums2[right]; //put nums2 in nums1 in sorted way
+                right--;
+            }
+            k--;
         }
-        Arrays.sort(nums1);
+        while(right>=0){ //put leftover nums2 array elements in nums1 array
+            nums1[k]=nums2[right];
+            k--;
+            right--;
+        }
+        // for(int i=0;i<n;i++){
+        //     nums1[m+i]=nums2[i];
+        // }
+        // Arrays.sort(nums1);
     }
 }
