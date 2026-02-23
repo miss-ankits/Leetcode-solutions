69f29f2ae0f9fe3aaf03699499f584cdc0cf1847
miss-ankits <81066165+miss-ankits@users.noreply.github.com>
2026-02-24 00:18:35 +0530

    Time: 4 ms (55.96%) | Memory: 43.9 MB (27.70%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,19 +1,32 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        int count=0; String result="";
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='('){
-                if(count>0){
-                    result+=s.charAt(i);
-                }
-                count++;
-            }else {
-                count--;
-            if(count>0){
-                result+=s.charAt(i);
-            }
+        //int count=0; String result="";
+    //     for(int i=0;i<s.length();i++){
+    //         if(s.charAt(i)=='('){
+    //             if(count>0){
+    //                 result+=s.charAt(i);
+    //             }
+    //             count++;
+    //         }else {
+    //             count--;
+    //         if(count>0){
+    //             result+=s.charAt(i);
+    //         }
+    //     }
+    // }
+    int count=0;
+    StringBuilder sb=new StringBuilder();
+    int start=0;int end=0;
+    while(end<s.length()){
+        if(s.charAt(end)=='('){
+            count ++;
+        }else count--;
+        if(count==0){
+            sb.append(s.substring(start+1,end));
+            start=end+1;
         }
+        end++;
     }
-        return result;
+        return sb.toString();
     }
 }
