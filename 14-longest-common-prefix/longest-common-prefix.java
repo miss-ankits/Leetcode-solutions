869567f4b3b91970a9e97089f3a8d0951ffa06a9
miss-ankits <81066165+miss-ankits@users.noreply.github.com>
2026-02-25 20:18:16 +0530

    Time: 1 ms (65.36%) | Memory: 43.4 MB (31.92%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -3,10 +3,10 @@ class Solution {
         if(strs.length==0 || strs==null) return "";
         StringBuilder sb=new StringBuilder();
         Arrays.sort(strs);
-        String first=strs[0];
-        String last= strs[strs.length-1];
+        String first=strs[0]; //first element
+        String last= strs[strs.length-1]; //last element
         for(int i=0;i<Math.min(first.length(),last.length());i++){
-            if(first.charAt(i)!=last.charAt(i)){
+            if(first.charAt(i)!=last.charAt(i)){ //compare char for prefix
                 return sb.toString();
             }
                 sb.append(first.charAt(i));
