c9aab74e304f6e5a7e10d02f4489dd39e16f192d
miss-ankits <81066165+miss-ankits@users.noreply.github.com>
2026-02-26 17:41:30 +0530

    Time: 16 ms (42.10%) | Memory: 43.8 MB (74.65%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length() != t.length()) return false;
+        Map<Character,Character> map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char sc=s.charAt(i);
+            char tc=t.charAt(i);
+            if(map.containsKey(sc)){
+                if(map.get(sc)!= tc){
+                return false;
+                }
+            }else if(map.containsValue(tc)){
+                return false;
+            }
+        map.put(sc,tc);
+    }
+    return true;
+}
+}
