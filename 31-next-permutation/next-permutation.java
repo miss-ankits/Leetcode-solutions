882280f2e8c869025b762d30cb5f196e5a2a1a55
miss-ankits <81066165+miss-ankits@users.noreply.github.com>
2026-07-13 17:55:20 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (69.46%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -2,31 +2,33 @@ class Solution {
     public void nextPermutation(int[] nums) {
         int n=nums.length-1;
         int ind=-1;
-        for(int i=n-1;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                ind=i;
+        for(int i=n;i>0;i--){ 
+            if(nums[i]>nums[i-1]){//compare from last to find the smallest number
+                ind=i-1;
                 break;
             }
         }
-        if(ind==-1){
+        if (ind==-1){ // if not found its the last combination,reverse to get the first
             reverse(nums,0,n);
-            return;
+            return ;
         }
-            for(int i=n;i>ind;i--){
-                if(nums[i]>nums[ind]){
-                    swap(nums,i,ind);
-                    break;
-                }
+        for(int i=n;i>ind;i--){// swap with the smallest no. from ind to n
+            if(nums[i]>nums[ind]){
+                swap(nums,i,ind);
+                break;
             }
-            reverse(nums,ind+1,n);
+        }
+        reverse(nums,ind+1,n);
     }
-    public void reverse(int [] nums,int start,int end){
+
+    private void reverse(int [] nums, int start,int end){
         while(start<end){
             swap(nums,start,end);
-            start++;end--;
+            start++;
+            end--;
         }
     }
-    public void swap(int [] nums,int start,int end){
+    private void swap(int [] nums, int start,int end){
         int temp=nums[start];
         nums[start]=nums[end];
         nums[end]=temp;
