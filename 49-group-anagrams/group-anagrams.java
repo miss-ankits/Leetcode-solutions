dc4efaca6807cddc47b5ca05e22eceec3ed5fdb5
miss-ankits <81066165+miss-ankits@users.noreply.github.com>
2026-08-06 01:58:15 +0530

    Time: 7 ms (72.30%) | Memory: 49.9 MB (25.12%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -3,14 +3,14 @@ class Solution {
         List<List<String>> ans=new ArrayList<>();
         Map<String,List<String>> map=new HashMap<>();
         for(String s: strs){
-            char[] arr=s.toCharArray();
-            Arrays.sort(arr); 
+            char[] arr=s.toCharArray(); //[ate,..,..] -> [a,t,e]
+            Arrays.sort(arr); //sort each string of the array by char
             String key=new String(arr);
         if(!map.containsKey(key)){
-            map.put(key,new ArrayList<>());
+            map.put(key,new ArrayList<>()); //new list to add string of same group
         }
         map.get(key).add(s);
         }
-        return new ArrayList<>(map.values());
+        return new ArrayList<>(map.values()); 
     }
 }
